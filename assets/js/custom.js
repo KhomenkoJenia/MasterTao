72fb7ede4e7fe2a5a1ec49320b82b513fbb20afc
JeniaKhomenko <jeniaaajeniaaa@gmail.com>
2023-03-09 01:38:49 +0200

    img

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,28 +1,37 @@
-$(document).ready(function(){
-  $('.banner').slick({
-    dots: true,
-    arrows: true,
-    appendArrows:".banner-nav",
-    responsive: [
-      {
-        breakpoint: 991,
-        settings: {
-          arrows:false,
-        }
-      },
-      
-    ]
-  });
-  
-  
+$(document).ready(function () {
+	$(".banner").slick({
+		dots: true,
+		arrows: true,
+		appendArrows: ".banner-nav",
+		responsive: [
+			{
+				breakpoint: 991,
+				settings: {
+					arrows: false,
+				},
+			},
+		],
+	});
+});
+const Section = document.querySelectorAll("section");
+const ContainerFluid = document.querySelectorAll("container-fluid");
+
+ContainerFluid.forEach((item) => {
+	const section = element.closest("section");
+
+	if (section.classList.contains("first-screen")) {
+		null;
+	} else {
+		section.closest("section").style.padding = "0 50px";
+	}
 });
 
-const btnBox = document.querySelectorAll('.input-checkbox');
+const btnBox = document.querySelectorAll(".input-checkbox");
 
-btnBox.forEach(item => {
-item.closest('.col-md-6').classList.add('button-group')
-})
+btnBox.forEach((item) => {
+	item.closest(".col-md-6").classList.add("button-group");
+});
 
-document.getElementById('fileInput').onchange = function () {
-document.getElementById('file-name').innerHTML = this.files[0].name;
+document.getElementById("fileInput").onchange = function () {
+	document.getElementById("file-name").innerHTML = this.files[0].name;
 };
